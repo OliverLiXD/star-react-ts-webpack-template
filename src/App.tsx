38242385f3d29779104695142aa9dc88d13f1ecc
laymanli <1009641543@qq.com>
2022-09-12 18:36:40 +0800

    V1.0 with formal template.

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
-import React, { lazy, Suspense, useState } from "react";
+import React from "react";
 
-import "@/App.scss";
-import Class from '@/components/Class';
-import img from "@/assets/img.png";
-import { Demo1, Demo2 } from "@/components";
-const LazyDemo = lazy(() => import(/* webpackPrefetch: true */"@/components/LazyDemo"));
+import "./App.scss"
 
 function App() {
-  const [show, setShow] = useState(false);
-
-  console.log('NODE_ENV', process.env.NODE_ENV);
-  console.log('BASE_ENV', process.env.BASE_ENV);
   return (
-    <h2>
-      <button onClick={() => setShow(!show)}>click me to show lazyload component</button>
-      <Demo1></Demo1>
-      star-react-ts-webpack-template
-      <img src={img} alt="" />
-      <div className="box"></div>
-      <Class></Class>
-      <span>11121</span>
-      {show ? <Suspense fallback={"somefallback"}><LazyDemo></LazyDemo></Suspense> : ""}
-    </h2>
+    <div className="App">
+      Hello, world!
+    </div>
   )
 }
 export default App;
--- src/components/LazyDemo.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-function LazyDemo() {
-  return <h3>我是LazyDemo组件</h3>
-}
-export default LazyDemo
